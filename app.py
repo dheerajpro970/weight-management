4422fc396c2c831b2bf87d4192945be61ec5949d
dheerajpro970 <dheerajpro970@gmail.com>
2025-10-29 11:41:11 +0530

    update code

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 print("this is in the new file")
+
+#enter the user input weight in kgs
+weight = int(input("Enter the weight in kgs:"))
+
+#enter user input height in cms
+height = int(input("Enter the height in cms:"))
+
+print(f"your weight is {weight}kgs and your height is {height}cms")
