78e27e0ca0c3ae279bd17e5f95b3f296f9ed9e8c
dheerajpro970 <dheerajpro970@gmail.com>
2025-10-30 11:22:46 +0530

    added the logic

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
+from logic import BMI
 print("this is in the new file")
 
 #enter the user input weight in kgs
-weight = int(input("Enter the weight in kgs:"))
+weight = float(input("Enter the weight in kgs:"))
 
 #enter user input height in cms
-height = int(input("Enter the height in cms:"))
+height = float(input("Enter the height in cms:"))
+
 
 print(f"your weight is {weight}kgs and your height is {height}cms")
+
+bmi = BMI(weight,height)
+print(f"Your BMI value {bmi}")
---
+++ logic.py
@@ -0,0 +1,4 @@
+#calculate the bmi value
+def BMI(weight, height):
+    bmi = weight/(height)**2
+    return bmi
